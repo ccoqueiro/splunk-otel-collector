07a83dbe20193f684f97f2e6235fd62f1823c727
Pablo Collins <pcollins@splunk.com>
2022-02-22 10:20:00 -0500

    zc: generate the service name; add readme

--- instrumentation/src/main_test.h
+++ instrumentation/src/main_test.h
@@ -1,23 +1,16 @@
 #ifndef SPLUNK_INSTRUMENTATION_TEST_MAIN_H
 #define SPLUNK_INSTRUMENTATION_TEST_MAIN_H
 
-#include <stdbool.h>
 #include "logger.h"
 #include "config.h"
+#include "cmdline_reader.h"
+#include <stdbool.h>
 
 static char *const java_tool_options_var_name = "JAVA_TOOL_OPTIONS";
 
 typedef void (test_func_t)(logger);
 
-void fake_load_config(logger log, struct config *cfg, char *path);
-
-void nop_load_config(logger log, struct config *cfg, char *path);
-
-void fail();
-
-void run_tests();
-
-void run_test(test_func_t f);
+cmdline_reader new_default_test_cmdline_reader();
 
 bool access_check_true(const char *s);
 
@@ -25,7 +18,9 @@ bool access_check_false(const char *s);
 
 void test_auto_instrument_not_java(logger l);
 
-void test_auto_instrument_success(logger l);
+void test_auto_instrument_svc_name_in_config(logger l);
+
+void test_auto_instrument_no_svc_name_in_config(logger l);
 
 void test_auto_instrument_no_access(logger l);
 
@@ -37,11 +32,51 @@ void test_auto_instrument_splunk_env_var_false_caps(logger l);
 
 void test_auto_instrument_splunk_env_var_zero(logger l);
 
-void test_auto_instrument_splunk_missing_file(logger l);
-
 void test_read_config(logger l);
 
 void test_read_config_missing_file(logger l);
+
+void test_read_args_simple(logger l);
+
+void test_read_args_limit(logger l);
+
+void test_extract_servicename_from_args_tomcat(logger l);
+
+void test_extract_servicename_from_args_simple_jar(logger l);
+
+void test_clean_jars(logger l);
+
+void test_clean_jar(logger l);
+
+void test_is_unique_path_element(logger l);
+
+void test_truncate_jar(logger l);
+
+void test_truncate_jar_short(logger l);
+
+void test_dots_to_dashes(logger l);
+
+void test_env_var_already_set(logger l);
+
+// helpers
+
+void fake_load_config(logger log, struct config *cfg, char *path);
+
+void fake_load_config_no_svcname(logger log, struct config *cfg, char *path);
+
+int tomcat_args(char *args[]);
+
+int petclinic_args(char *args[]);
+
+void fail();
+
+// test utils
+
+void run_tests();
+
+void run_test(test_func_t run_test);
+
+void require_true(char *funcname, bool actual);
 
 void require_equal_ints(char *funcname, int expected, int actual);
 
--- instrumentation/src/splunk.h
+++ instrumentation/src/splunk.h
@@ -1,11 +1,10 @@
 #ifndef SPLUNK_INSTRUMENTATION_SPLUNK_H
 #define SPLUNK_INSTRUMENTATION_SPLUNK_H
 
-#include <stdbool.h>
 #include "logger.h"
 #include "config.h"
-
-static char *const conf_file = "/usr/lib/splunk-instrumentation/auto-instr.conf";
+#include "cmdline_reader.h"
+#include <stdbool.h>
 
 static char *const disable_env_var_name = "DISABLE_SPLUNK_AUTOINSTRUMENTATION";
 
@@ -13,7 +12,13 @@ typedef bool (*has_access_func_t)(const char *);
 
 typedef void (*load_config_func_t)(logger log, struct config *, char *);
 
-void auto_instrument(logger log, has_access_func_t has_access, const char *program_name, load_config_func_t load_config_func);
+void auto_instrument(
+        logger log,
+        has_access_func_t has_access,
+        const char *program_name,
+        load_config_func_t load_config_func,
+        cmdline_reader cr
+);
 
 bool streq(const char *expected, const char *actual);
 
